4b71b6a8f59cf25c60143ea4eea70be3efee8fa4
Ben Longden <ben@nocarrier.co.uk>
2012-09-13 10:34:44 -0400

    Change short array syntax for PHP5.3 compatibility

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 $app = new Silex\Application();
 
 $app->get('/', function() use ($app) {
-    $res = new Response('<ticket>Stuff</ticket>', 200, [ 'Content-Type' => 'application/vnd.org.restfest.hackday2012+xml' ]);
+    $res = new Response('<ticket>Stuff</ticket>', 200, array('Content-Type' => 'application/vnd.org.restfest.hackday2012+xml'));
     return $res;
 });
 
